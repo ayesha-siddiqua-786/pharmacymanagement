38d4423aa02b40ac43a48c61bb726d50409a6ecb
ayesha-siddiqua-786 <144307790+ayesha-siddiqua-786@users.noreply.github.com>
2024-02-12 12:52:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,127 +1,214 @@
-<!DOCTYPE html>
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(isset($_POST['signin']))
+{
+$uname=$_POST['username'];
+$password=md5($_POST['password']);
+$sql ="SELECT EmailId,Password,Status,id FROM tblemployees WHERE EmailId=:uname and Password=:password";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':uname', $uname, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+ foreach ($results as $result) {
+    $status=$result->Status;
+    $_SESSION['eid']=$result->id;
+  }
+if($status==0)
+{
+$msg="Your acc is Inactive. Please contact admin";
+} else{
+$_SESSION['emplogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location = 'myprofile.php'; </script>";
+} }
+
+else{
+
+  echo "<script>alert('Invalid Details');</script>";
+
+}
+
+}
+
+?><!DOCTYPE html>
 <html lang="en">
-    <?php
-    session_start();
-    ob_start();
-    include('header.php');
-    include('admin/db_connect.php');
+    <head>
 
-	$query = $conn->query("SELECT * FROM system_settings limit 1")->fetch_array();
-	foreach ($query as $key => $value) {
-		if(!is_numeric($key))
-			$_SESSION['setting_'.$key] = $value;
-	}
-    ob_end_flush();
-    ?>
+        <!-- Title -->
+        <title>HLM | Home Page</title>
 
-    <style>
-    	header.masthead {
-		  background: url(assets/img/<?php echo $_SESSION['setting_cover_img'] ?>);
-		  background-repeat: no-repeat;
-		  background-size: cover;
-		}
-    </style>
-    <body id="page-top">
-        <!-- Navigation-->
-        <div class="toast" id="alert_toast" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-body text-white">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        <meta charset="UTF-8">
+        <meta name="description" content="Responsive Admin Dashboard Template" />
+        <meta name="keywords" content="admin,dashboard" />
+        <meta name="au" content="Steelcoders" />
+
+        <!-- Styles -->
+        <link type="text/css" rel="stylesheet" href="assets/plugins/materialize/css/materialize.min.css"/>
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="assets/plugins/material-preloader/css/materialPreloader.min.css" rel="stylesheet">
+
+
+        <!-- Theme Styles -->
+        <link href="assets/css/alpha.min.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
+
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="http://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="http://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+    <body>
+        <div class="loader-bg"></div>
+        <div class="loader">
+            <div class="preloader-wrapper big active">
+                <div class="spinner-layer spinner-blue">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div><div class="gap-patch">
+                    <div class="circle"></div>
+                    </div><div class="circle-clipper right">
+                    <div class="circle"></div>
+                    </div>
+                </div>
+                <div class="spinner-layer spinner-spinner-teal lighten-1">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div><div class="gap-patch">
+                    <div class="circle"></div>
+                    </div><div class="circle-clipper right">
+                    <div class="circle"></div>
+                    </div>
+                </div>
+                <div class="spinner-layer spinner-yellow">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div><div class="gap-patch">
+                    <div class="circle"></div>
+                    </div><div class="circle-clipper right">
+                    <div class="circle"></div>
+                    </div>
+                </div>
+                <div class="spinner-layer spinner-green">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div><div class="gap-patch">
+                    <div class="circle"></div>
+                    </div><div class="circle-clipper right">
+                    <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="./"><?php echo $_SESSION['setting_name'] ?></a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=home">Home</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=flights"></span>Flight List</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php?page=about">About</a></li>
-                        
-                     
-                    </ul>
-                </div>
-            </div>
-        </nav>
-       
-        <?php 
-        $page = isset($_GET['page']) ?$_GET['page'] : "home";
-        include $page.'.php';
-        ?>
-       
+        <div class="mn-content fixed-sidebar">
+            <header class="mn-header navbar-fixed">
+                <nav class="cyan darken-1">
+                    <div class="nav-wrapper row">
+                        <section class="material-design-hamburger navigation-toggle">
+                            <a href="#" data-activates="slide-out" class="button-collapse show-on-large material-design-hamburger__icon">
+                                <span class="material-design-hamburger__layer"></span>
+                            </a>
+                        </section>
+                        <div class="header-title col s3">
+                            <span class="chapter-title"></span>
+                        </div>
 
-<div class="modal fade" id="confirm_modal" role='dialog'>
-    <div class="modal-dialog modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title">Confirmation</h5>
-      </div>
-      <div class="modal-body">
-        <div id="delete_content"></div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='confirm' onclick="">Continue</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="uni_modal" role='dialog'>
-    <div class="modal-dialog modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title"></h5>
-      </div>
-      <div class="modal-body">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id='submit' onclick="$('#uni_modal form').submit()">Save</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal fade" id="uni_modal_right" role='dialog'>
-    <div class="modal-dialog modal-full-height  modal-md" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-        <h5 class="modal-title"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span class="fa fa-arrow-righ t"></span>
-        </button>
-      </div>
-      <div class="modal-body">
-      </div>
-      </div>
-    </div>
-  </div>
-  <div id="preloader"></div>
-        <footer class="bg-light py-5">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8 text-center">
-                        <h2 class="mt-0">Contact us</h2>
-                        <hr class="divider my-4" />
+
+                        </form>
+
+
                     </div>
+                </nav>
+            </header>
+
+
+            <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+
+
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion" style="">
+                    <li>&nbsp;</li>
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="index.php"><i class="material-icons"></i>Student Login</a></li>
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="forgot-password.php"><i class="material-icons"></i>Stu Pwsd Recovery</a></li>
+
+                       <li class="no-padding"><a class="waves-effect waves-grey" href="admin/"><i class="material-icons"></i>Warden Login</a></li>
+                       <li class="no-padding"><a class="waves-effect waves-grey" href="hod/hodindex.php"><i class="material-icons"></i>Hod Login</a></li>
+                       <li class="no-padding"><a class="waves-effect waves-grey" href="gate\gateindex.php"><i class="material-icons"></i>Gate Login</a></li>
+
+                </ul>
+          <div class="footer">
+                    
+
                 </div>
+                </div>
+            </aside>
+            <main class="mn-inner">
                 <div class="row">
-                    <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
-                        <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
-                        <div><?php echo $_SESSION['setting_contact'] ?></div>
-                    </div>
-                    <div class="col-lg-4 mr-auto text-center">
-                        <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
-                        <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
-                        <a class="d-block" href="mailto:<?php echo $_SESSION['setting_email'] ?>"><?php echo $_SESSION['setting_email'] ?></a>
+                    <div class="col s12">
+                        <div class="page-title"><b><h4>Welcome to Girls Hostel Leave Automation</h4><b></div>
+
+                          <div class="col s12 m6 l8 offset-l2 offset-m3">
+                              <div class="card white darken-1">
+
+                                  <div class="card-content ">
+                                      <span class="card-title" style="font-size:20px;">Student Login</span>
+                                         <?php if($msg){?><div class="errorWrap"><strong>Error</strong> : <?php echo htmlentities($msg); ?> </div><?php }?>
+                                       <div class="row">
+                                           <form class="col s12" name="signin" method="post">
+                                               <div class="input-field col s12">
+                                                   <input id="username" type="text" name="username" class="validate" autocomplete="off" required >
+                                                   <label for="email">Email Id</label>
+                                               </div>
+                                               <div class="input-field col s12">
+                                                   <input id="password" type="password" class="validate" name="password" id="password" autocomplete="off" required>
+                                                   <label for="password">Password</label>
+                                                   </div>
+
+                                                   <div class="input-field col s12">
+                                                   <input type="checkbox" onclick="myFunction()" id="check" name="check"></span>
+                                                    <label for="check">Show pwd</label>
+                                                    </div>
+                                               
+                                               <div class="col s12 right-align m-t-sm">
+
+                                                   <input type="submit" name="signin" value="Sign in" class="waves-effect waves-light btn teal">
+                                               </div>
+                                           </form>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
                     </div>
                 </div>
-            </div>
-            <br>
-            <div class="container"><div class="small text-center text-muted"> <?php echo $_SESSION['setting_name'] ?> | <a href="https://www.campcodes.com" target="_blank">CampCodes</a></div></div>
-        </footer>
-        
-       <?php include('footer.php') ?>
+            </main>
+
+        </div>
+        <div class="left-sidebar-hover"></div>
+
+        <!-- Javascripts -->
+        <script src="assets/plugins/jquery/jquery-2.2.0.min.js"></script>
+        <script src="assets/plugins/materialize/js/materialize.min.js"></script>
+        <script src="assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
+        <script src="assets/plugins/jquery-blockui/jquery.blockui.js"></script>
+        <script src="assets/js/alpha.min.js"></script>
+        <script>
+function myFunction() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+</script>
+
     </body>
-
-    <?php $conn->close() ?>
-
 </html>
